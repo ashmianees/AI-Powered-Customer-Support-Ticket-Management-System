56bfd4ec3677722d747cfcca41679950b7d0ba91
ashmi9847 <ashmianees2025@mca.ajce.in>
2026-08-02 14:38:29 +0530

    change vite

--- client/vite.config.js
+++ client/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://ai-powered-customer-support-ticket-six.vercel.app',
         changeOrigin: true
       }
     }
